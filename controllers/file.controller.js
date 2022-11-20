edfbe1fe4b93b03a31a9eebef45bd3c2eb9d49fd
Khamitamantaev <32989550+Khamitamantaev@users.noreply.github.com>
2022-11-20 11:23:51 +0500

    add mkdir if file not exist 'resources'

--- controllers/file.controller.js
+++ controllers/file.controller.js
@@ -1,18 +1,26 @@
 const path = require('path')
+const fs = require('fs');
 exports.upload = (req, res) => {
     console.log(req.files)
     if (!req.files || Object.keys(req.files).length === 0) {
         return res.status(400).send('No files uploaded.');
-      }
-      let sampleFile =  req.files.simple;
-        //   const fulleName = sampleFile.name.slice(0, sampleFile.name.lastIndexOf("."));
-      const mainpath = path.join(__dirname, '..', 'resources', sampleFile.name)
+    }
+
+    let sampleFile = req.files.simple;
+
+    const mainpath = path.join(__dirname, '..', 'resources', sampleFile.name)
+
+    if (!fs.existsSync(path.join(__dirname, '..', 'resources'))) {
+        fs.mkdirSync(path.join(__dirname, '..', 'resources'));
+    }
+    //   const fulleName = sampleFile.name.slice(0, sampleFile.name.lastIndexOf("."));
     
-      sampleFile.mv(mainpath, function(err) {
+
+    sampleFile.mv(mainpath, function (err) {
         if (err)
-          return res.status(500).send(err);
+            return res.status(500).send(err);
         res.send('File uploaded!');
-      });
+    });
 };
 
 exports.list = (req, res) => {
